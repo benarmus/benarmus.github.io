cf1af67b686bfefb8b18b8c7a5405f85e74a7532
benarmus <benarmus@utexas.edu>
2026-04-14 00:07:45 +0000

    Deploying to gh-pages from @ benarmus/benarmus.github.io@c3f2c137915882dad458dbd516412eb3554a0c61 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "benarmus/BenArmus",
+    "data-repo": "benarmus/benarmus.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
